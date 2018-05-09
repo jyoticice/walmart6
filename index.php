b9cc76cf4020bd7efc62dcdc8e01d82bf26f5d6f
jyoticice <39135407+jyoticice@users.noreply.github.com>
2018-05-10 00:21:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.amazon.com/gp/product/B004QWYCVG/ref=s9_acsd_top_hd_bw_bjxvN5_c_x_1_w/131-9022633-5815233?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-5&pf_rd_r=S35BKXN0ZBXYFJQFH5J2&pf_rd_t=101&pf_rd_p=eb1b2e2c-ce5e-5777-a67f-63cfa69505b3&pf_rd_i=679217011");
 die();
 ?>
